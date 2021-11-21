6a8ca7ff0452943b4341e316f42066e173c5327c
BuushA <user@user-PC>
2021-11-21 13:39:23 +0200

    Iron Sickle atempt

---
+++ src/main/java/net/mcreator/testmod/procedures/IronSickleBlockDestroyedWithToolProcedure.java
@@ -0,0 +1,133 @@
+package net.mcreator.testmod.procedures;
+
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.common.PlantType;
+import net.minecraftforge.common.IPlantable;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.state.Property;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.testmod.TestmodMod;
+
+import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
+
+public class IronSickleBlockDestroyedWithToolProcedure {
+	@Mod.EventBusSubscriber
+	private static class GlobalTrigger {
+		@SubscribeEvent
+		public static void onBlockBreak(BlockEvent.BreakEvent event) {
+			Entity entity = event.getPlayer();
+			IWorld world = event.getWorld();
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("xpAmount", event.getExpToDrop());
+			dependencies.put("x", event.getPos().getX());
+			dependencies.put("y", event.getPos().getY());
+			dependencies.put("z", event.getPos().getZ());
+			dependencies.put("px", entity.getPosX());
+			dependencies.put("py", entity.getPosY());
+			dependencies.put("pz", entity.getPosZ());
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("blockstate", event.getState());
+			dependencies.put("event", event);
+			executeProcedure(dependencies);
+		}
+	}
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				TestmodMod.LOGGER.warn("Failed to load dependency x for procedure IronSickleBlockDestroyedWithTool!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				TestmodMod.LOGGER.warn("Failed to load dependency y for procedure IronSickleBlockDestroyedWithTool!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				TestmodMod.LOGGER.warn("Failed to load dependency z for procedure IronSickleBlockDestroyedWithTool!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				TestmodMod.LOGGER.warn("Failed to load dependency world for procedure IronSickleBlockDestroyedWithTool!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((new Object() {
+			public boolean checkPlantType(IWorld world, BlockPos pos) {
+				Block _block = world.getBlockState(pos).getBlock();
+				return _block instanceof IPlantable ? ((IPlantable) _block).getPlantType(world, pos) == PlantType.CROP : false;
+			}
+		}.checkPlantType(world, new BlockPos((int) x, (int) y, (int) z)))) {
+			if (world instanceof World)
+				((World) world).notifyNeighborsOfStateChange(new BlockPos((int) x, (int) y, (int) z),
+						((World) world).getBlockState(new BlockPos((int) x, (int) y, (int) z)).getBlock());
+			{
+				BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+				BlockState _bs = Blocks.WHEAT.getDefaultState();
+				BlockState _bso = world.getBlockState(_bp);
+				for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+					Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+					if (_property != null && _bs.get(_property) != null)
+						try {
+							_bs = _bs.with(_property, (Comparable) entry.getValue());
+						} catch (Exception e) {
+						}
+				}
+				TileEntity _te = world.getTileEntity(_bp);
+				CompoundNBT _bnbt = null;
+				if (_te != null) {
+					_bnbt = _te.write(new CompoundNBT());
+					_te.remove();
+				}
+				world.setBlockState(_bp, _bs, 3);
+				if (_bnbt != null) {
+					_te = world.getTileEntity(_bp);
+					if (_te != null) {
+						try {
+							_te.read(_bso, _bnbt);
+						} catch (Exception ignored) {
+						}
+					}
+				}
+			}
+			{
+				TileEntity _ent = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+				if (_ent != null) {
+					final int _sltid = (int) (0);
+					final int _amount = (int) 1;
+					_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+						if (capability instanceof IItemHandlerModifiable) {
+							ItemStack _stk = capability.getStackInSlot(_sltid).copy();
+							if (_stk.attemptDamageItem(_amount, new Random(), null)) {
+								_stk.shrink(1);
+								_stk.setDamage(0);
+							}
+							((IItemHandlerModifiable) capability).setStackInSlot(_sltid, _stk);
+						}
+					});
+				}
+			}
+		}
+	}
+}
